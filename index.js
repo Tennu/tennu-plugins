0dbc5448168267fed7525e59e69f181eeb55e0c7
Ryan Scheel <ryan.havvy@gmail.com>
2013-06-26 16:03:10 -0700

    Bugs fixed. 1.0 release.

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
 var path = require('path');
 var here = path.resolve(__dirname, __filename);
 var root = path.resolve('/');
-var exists = require('fs').existsSync;
+var fs_exists = require('fs').existsSync;
+var exists = function (p) {
+    return fs_exists(p) || fs_exists(p + ".js");
+};
+var debug = function (msg) {
+    if (false) console.log("Modules: " + msg);
+};
 
 module.exports = function Modules (subscriber, context) {
     var exports = {};
     var loaded = [];
     var loading = [];
 
-    // Takes a module name, and goes up the directory chain from where
-    // this file is located, checking for existance of modules, first in
-    // ./tennu_modules/%name%, and then in ./node_modules/tennu-%name%.
-    function require (name) {
-        if (isLoaded(name)) { return exports[name]; }
-        var there = here;
+    function tennu_require (name) {
+        debug("Requiring " + name);
+
+        if (!isLoaded(name)) {
+            debug("Module not required yet. Locating");
+            var load_path = locate(name);
+            load(require(load_path));
+            debug("Module located.");
+        }
+
+        return exports[name];
+    }
+
+    function locate (name) {
+        var there = here, tennu_path, node_path;
 
         while (there !== root) {
             there = path.resolve(there, '../');
 
-            if (exists(path.resolve(there, 'tennu_modules', name))) {
-                load(require(path.resolve(there, './tennu_modules', name)), name);
-                return exports[name];
-            }
+            tennu_path = path.resolve(there, 'tennu_modules', name);
+            debug("Searching at: " + tennu_path);
+            if (exists(tennu_path)) { return tennu_path; }
 
-            if (exists(path.resolve(there, 'node_modules', 'tennu-' + name))) {
-                load(require(path.resolve(there, 'node_modules', 'tennu-' + name)), name);
-                return exports[name];
-            }
+            node_path = path.resolve(there, 'node_modules', 'tennu-' + name);
+            debug("Searching at: " + node_path);
+            if (exists(node_path)) { return node_path; }
         }
 
-        throw new Error("Failed to require Nark module " + name);
+        throw new Error("Failed to require Tennu module '" + name + "'");
     }
 
     function initialize (Module, name) {
         // Create the actual module.
         var module = new Module(context);
 
-        // Make sure the module has imports and exports objects.
         module.exports = module.exports || {};
         module.imports = {};
 
@@ -44,17 +56,21 @@ module.exports = function Modules (subscriber, context) {
             loadDependencies(module.dependencies, module.imports, name);
         }
 
-        // Listen to Handler Events
-        Object.keys(module.handlers).forEach(function (event) {
-            var listener = module.handlers[event];
-            subscriber.on(event, listener);
+        if (module.handlers) {
+            subscribeHandlers(module.handlers);
+        }
+    }
+
+    function subscribeHandlers (handlers) {
+        Object.keys(handlers).forEach(function (event) {
+            subscriber.on(event, handlers[event]);
         });
     }
 
     function loadDependencies(deps, imports, name) {
         deps.forEach(function (dep) {
             try {
-                imports[dep] = require(dep);
+                imports[dep] = tennu_require(dep);
             } catch (e) {
                 throw new Error("Failed to find Nark Module dependency " + dep + " for " + name);
             }
@@ -92,7 +108,7 @@ module.exports = function Modules (subscriber, context) {
     }
 
     return {
-        require : require,
+        require : tennu_require,
         load: load,
         isLoaded : isLoaded,
         loadedModules : loadedModules,
